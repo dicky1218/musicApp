e036acf1b5bde05df83bb68cf5c95f6a44614b9d
CK Yuen <dicky1218@hotmail.com.hk>
2022-06-03 23:53:21 +0800

    updatelogo

--- app/src/main/java/com/kotlin/musicplayer/LoginActivity.kt
+++ app/src/main/java/com/kotlin/musicplayer/LoginActivity.kt
@@ -1,4 +1,29 @@
 package com.kotlin.musicplayer
 
-class LoginActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseUser
+
+class LoginActivity : AppCompatActivity() {
+
+    var currentUser : FirebaseUser ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (currentUser != null) {
+            sendToMainActivity()
+        }
+    }
+
+    private fun sendToMainActivity() {
+        var intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+    }
 }
--- app/src/main/java/com/kotlin/musicplayer/MainActivity.kt
+++ app/src/main/java/com/kotlin/musicplayer/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.kotlin.musicplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jean.jcplayer.model.JcAudio
 import com.example.jean.jcplayer.view.JcPlayerView
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var player : JcPlayerView
+
+    var currentUser : FirebaseUser ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,4 +31,16 @@ class MainActivity : AppCompatActivity() {
         player.createNotification()
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        if (currentUser == null) {
+            sendToLoginActivity()
+        }
+    }
+
+    private fun sendToLoginActivity() {
+        var intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+    }
 }
