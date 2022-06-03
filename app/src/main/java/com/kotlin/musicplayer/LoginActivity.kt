946c46870c1bbb7f30b9911cc0fb9f71aa398730
CK Yuen <dicky1218@hotmail.com.hk>
2022-06-03 23:11:25 +0800

    workonlogin

---
+++ app/src/main/java/com/kotlin/musicplayer/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.kotlin.musicplayer
+
+class LoginActivity {
+}
--- app/src/main/java/com/kotlin/musicplayer/MainActivity.kt
+++ app/src/main/java/com/kotlin/musicplayer/MainActivity.kt
@@ -13,13 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val url1 = "https://firebasestorage.googleapis.com/v0/b/musicplayer-147fc.appspot.com/o/Naul%20%20-%20Memory%20Of%20The%20Wind%20%5Bmp3clan.com%5D.mp3?alt=media&token=41a84036-d4e2-40b9-927a-930bb84e9309"
+        val url2 = "https://firebasestorage.googleapis.com/v0/b/musicplayer-147fc.appspot.com/o/01.%20%EA%BD%83%EA%B8%B8%20(Prod.%20By%20%EC%A7%80%EC%BD%94(ZICO)).mp3?alt=media&token=51ed2724-cf90-40ba-928f-f00924527ce1"
+
         player = findViewById(R.id.jcplayer)
 
         val jcAudios: ArrayList<JcAudio> = ArrayList()
-        jcAudios.add(JcAudio.createFromURL("url audio", "http://xxx/audio.mp3"))
+        jcAudios.add(JcAudio.createFromURL("Memory of the wind", url1))
+        jcAudios.add(JcAudio.createFromURL("Flower Road", url2))
 
         player.initPlaylist(jcAudios, null)
-    }
 
+        player.createNotification()
+    }
 
 }
